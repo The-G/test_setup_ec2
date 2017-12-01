f35b44407d7ffb352927889b9a6c842cc035274c
ji hyuk kim <gskjhj@gmail.com>
2017-12-01 16:50:05 +0900

    hotfix:: post.rb

--- db/migrate/20171201093621_create_posts.rb
+++ db/migrate/20171201093621_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :title, null: false, default: ""
-      t.text :content, null: false, default: ""
+      t.string :title, null: false, default: "no title"
+      t.text :content, null: false, default: "no content"
       t.references :user, index: true, foreign_key: true
       t.timestamps null: false
     end
