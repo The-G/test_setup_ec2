ebd089c4d60968145b6c31545b438e9289671a7d
Ubuntu <ubuntu@ip-172-26-5-51.ap-northeast-1.compute.internal>
2017-12-01 07:41:46 +0000

    to merge

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20171201104430) do
   create_table "posts", force: :cascade do |t|
     t.string   "title",      default: "", null: false
     t.text     "content",    default: "", null: false
-    t.integer  "user_id",                 null: false
+    t.integer  "user_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
