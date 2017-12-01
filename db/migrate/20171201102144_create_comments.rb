2f904036f84ee6d257c7966b14412d83f32c5a11
ji hyuk kim <gskjhj@gmail.com>
2017-12-01 17:24:10 +0900

    hotfix:: edit migration to apply mysql

--- db/migrate/20171201102144_create_comments.rb
+++ db/migrate/20171201102144_create_comments.rb
@@ -3,7 +3,6 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.text :content, null: false, default: ""
       t.references :post, index: true, foreign_key: true
-      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
