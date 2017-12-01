f9a550f584c4a8334a65d6ec999d466d7265e23f
Ubuntu <ubuntu@ip-172-26-5-51.ap-northeast-1.compute.internal>
2017-12-01 08:31:55 +0000

    hotfix:: edit&add some migration to apply mysql2

--- db/migrate/20171201093621_create_posts.rb
+++ db/migrate/20171201093621_create_posts.rb
@@ -1,9 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :title, null: false, default: "no title"
-      t.text :content, null: false, default: "no content"
-      t.references :user, index: true, foreign_key: true
+      t.string :title, null: false
+      t.text :content, null: false
       t.timestamps null: false
     end
   end
